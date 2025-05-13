7e16abc955fd3033401db2fd5dcb495ede7528aa
betisbettter <em3yoga@gmail.com>
2025-05-13 15:59:28 +0000

    updated db

--- db.py
+++ db.py
@@ -1,11 +1,9 @@
+# db.py
 import psycopg2
-import os
+import streamlit as st
 
 def get_connection():
     return psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        host=os.getenv("DB_HOST"),
+        st.secrets["database"]["url"],
         sslmode="require"
     )
