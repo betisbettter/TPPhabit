d5db88dd4fdaca7bf97cde0497f298036ccbc255
betisbettter <em3yoga@gmail.com>
2025-05-13 11:18:41 -0400

    Create habit_logic.py

---
+++ habit_logic.py
@@ -0,0 +1,7 @@
+# Example list of 30 habits
+habit_list = [
+    "Drink 8oz of water", "Stretch for 5 minutes", "10 push-ups", # ...
+]
+
+def get_habits_for_day(day_number):
+    return habit_list[:day_number]
